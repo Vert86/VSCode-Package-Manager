50b441238fe10f11b9394d5ec2a3f91349ad7576
Vert86 <calvertedwards@gmail.com>
2023-11-05 20:24:20 +0800

    Updated extension.js to correctly reference bundle.js

--- src/extension.ts
+++ src/extension.ts
@@ -3,34 +3,37 @@ import * as path from 'path';
 
 export function activate(context: vscode.ExtensionContext) {
   let disposable = vscode.commands.registerCommand('testing-pp.showPanel', () => {
-    // This creates a new webview panel
+    // Create a new webview panel
     const panel = vscode.window.createWebviewPanel(
       'panel', // Identifies the type of the webview
       'Panel', // Title of the panel displayed to the user
       vscode.ViewColumn.One, // Editor column to show the new webview panel in.
-      { enableScripts: true } // Additional options. Here we enable JavaScript.
+      {
+        // Webview options
+        enableScripts: true
+      }
     );
 
     // Get the path on disk for the bundled React app
     const scriptPathOnDisk = vscode.Uri.file(
-      path.join(context.extensionPath, 'dist', 'bundle.js')
+      path.join(context.extensionPath, 'src', 'frontend', 'React_app', 'dist', 'bundle.js')
     );
 
     // Convert the disk path to a URI for use in the webview
     const scriptSrc = panel.webview.asWebviewUri(scriptPathOnDisk);
 
-    // Use a nonce for Content Security Policy (CSP)
-    const nonce = getNonce();
-
     // Set the HTML content for the webview with the script source
-    panel.webview.html = getWebviewContent(nonce, scriptSrc);
+    panel.webview.html = getWebviewContent(scriptSrc, context.extensionUri);
   });
 
   context.subscriptions.push(disposable);
 }
 
-function getWebviewContent(nonce: string, scriptSrc: vscode.Uri): string {
-  // Important: Ensure that the nonce and scriptSrc are used in the CSP and script tag respectively
+// You need to update the signature of getWebviewContent to accept scriptSrc
+function getWebviewContent(scriptSrc: vscode.Uri, extensionUri: vscode.Uri): string {
+  const nonce = getNonce();
+
+  // The nonce and scriptSrc are used in the CSP and the script tag respectively
   return `<!DOCTYPE html>
     <html lang="en">
     <head>
@@ -40,8 +43,8 @@ function getWebviewContent(nonce: string, scriptSrc: vscode.Uri): string {
       <meta name="viewport" content="width=device-width, initial-scale=1.0">
       <title>Panel</title>
     </head>
-	<h1 style="text-align:center">Welcome to Calvert's file Pack and Un-Pack Manager</h1>
     <body>
+      <h1 style="text-align:center">Welcome to Calvert's file Pack and Un-Pack Tool Extension</h1>
       <div id="root"></div>
       <!-- Use the scriptSrc for the React app's bundled script -->
       <script nonce="${nonce}" src="${scriptSrc}"></script>
@@ -50,7 +53,7 @@ function getWebviewContent(nonce: string, scriptSrc: vscode.Uri): string {
 }
 
 function getNonce(): string {
-  // This function generates a nonce for the CSP
+  // Generates a nonce for the CSP
   let text = '';
   const possible = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
   for (let i = 0; i < 32; i++) {
